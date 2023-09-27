82f6ae54b7807c3fbc88282744d27e20c1f628c0
vjcitn <stvjc@channing.harvard.edu>
2023-09-27 09:16:27 -0400

    ingesting training stats

--- R/graph_acts.R
+++ R/graph_acts.R
@@ -4,7 +4,8 @@
 #' @param copy logical(1) passed to gen_graph in simba.tl module, defaults
 #' to FALSE
 #' @param use_hvg logical(1) passed as `use_highly_variable` to gen_graph, defaults to FALSE
-#' @param dirname character(1) defaults to 'graph0'
+#' @param dirname character(1) defaults to 'graph0'; training stats will be saved
+#' to `paste0(simba_ref$settings$workdir, "/", dirname, "/", model, "/", "training_stats.json")`
 #' @param auto_wd logical(1) defaults to TRUE, for automatic setting of weight decay
 #' @param save_wd logical(1) defaults to TRUE
 #' @param output character(1) defaults to 'model'
@@ -31,3 +32,54 @@ build_and_train_pbg = function( adSimb, copy=FALSE,
  simba_ref$tl$pbg_train(auto_wd=auto_wd, save_wd=save_wd,
     output=output)
 }
+
+#' Helper function to recursively flatten nested lists
+#' produced by Chat-GPT 3
+flatten_json <- function(x, prefix = "") {
+  out <- list()
+  for (key in names(x)) {
+    new_key <- if (prefix == "") key else paste(prefix, key, sep = "_")
+    if (is.list(x[[key]])) {
+      out <- c(out, flatten_json(x[[key]], new_key))
+    } else {
+      out[[new_key]] <- x[[key]]
+    }
+  }
+  return(out)
+}
+
+#' get pbg_metrics data frames
+#' @importFrom jsonlite fromJSON
+#' @param simba_ref reference to simba module
+#' @param gg_dirname character(1) the dirname specified for build_and_train_pbg,
+#' defaults to 'graph0'
+#' @param tr_output character(1) the output specified for build_and_train_pbg
+#' defaults to 'model'
+#' @return a list with two data.frames of training statistics
+#' @examples
+#' p15 = get_paul15(overwrite=TRUE) # allow repetition
+#' ref = simba_ref()
+#' pp = ref$read_h5ad(p15)
+#' bb = basic_preproc(pp, simba_ref=ref)
+#' gg = build_and_train_pbg( bb, simba_ref=ref )
+#' ts = ingest_training_stats(ref)
+#' lapply(ts, head, 2)
+#' @export
+ingest_training_stats = function(simba_ref,
+  gg_dirname = 'graph0', tr_output = 'model') {
+  jsondir = sprintf(
+     paste0(simba_ref$settings$workdir, "/pbg/%s/%s/training_stats.json"),
+     gg_dirname, tr_output)
+  txt = readLines(jsondir)
+  parsed_stats = lapply(txt, jsonlite::fromJSON)
+  # seems to be alternating document schemata
+  ntxt = length(parsed_stats)
+  odd = seq(1, ntxt, 2)
+  ev = seq(2, ntxt, 2)
+  df1 = do.call(rbind, lapply(parsed_stats[odd], 
+     function(x) data.frame(flatten_json(x)))) 
+  df2 = do.call(rbind, lapply(parsed_stats[ev], 
+     function(x) data.frame(flatten_json(x)))) 
+  list(df1=df1, df2=df2)
+}
+ 
